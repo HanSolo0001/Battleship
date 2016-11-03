bd330200a2bbf1050ea357ced6dbb4f1913af970
HanSolo0001 <pillarite@gmail.com>
2016-11-03 19:34:19 -0400

    Intial commit

---
+++ src/Battleship/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Battleship.Startup))]
+namespace Battleship
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
